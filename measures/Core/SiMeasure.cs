587db207e8999ce125e11fba0fa409b7f4dc615b
Thomas Kägi <kaegit@gmail.com>
2020-06-25 17:11:56 +0200

    Simplify handling of dimensions.

--- measures/Core/SiMeasure.cs
+++ measures/Core/SiMeasure.cs
@@ -7,21 +7,33 @@ namespace Quantities.Measures.Core
     public abstract class SiMeasure : SiUnit, INormalise
     {
         internal abstract Normaliser Anchor { get; }
-        public Double Renormalise(in Double value) => Anchor.Renormalise(in value);
-        public Double Normalise(in Double value) => Anchor.Normalise(in value);
-        public Double Scale<TOther>(in Double other) where TOther : SiMeasure, new() => Anchor.Scale(Pool<TOther>.Item.Anchor, in other);
+        public abstract Double Renormalise(in Double value);
+        public abstract Double Normalise(in Double value);
+        internal abstract Double Scale(SiMeasure measure, in Double other);
     }
-    internal abstract class SiMeasure<TDim, TSiUnit> : SiMeasure
+    internal class SiMeasure<TDim, TSiUnit> : SiMeasure
         where TDim : Dimension, new()
         where TSiUnit : SiUnit, new()
     {
         private static readonly TSiUnit UNIT = Pool<TSiUnit>.Item;
-        private static readonly Normaliser<TDim> NORMALIZER = Normalisers<TDim>.Get(UNIT.Offset);
+        private static readonly Normaliser NORMALIZER = Normalisers.Get(UNIT.Offset);
         private static readonly String REPRESENTATION = $"{UNIT}{Pool<TDim>.Item}";
         internal override Int32 Offset => UNIT.Offset;
         internal override Normaliser Anchor => NORMALIZER;
 
         public override String ToString() => REPRESENTATION;
         internal static void Inject(IPrefixInjectable injectable) => NORMALIZER.InjectPrefix(injectable);
+        public override Double Renormalise(in Double value)
+        {
+            return NORMALIZER.Renormalise<TDim>(in value);
+        }
+        public override Double Normalise(in Double value)
+        {
+            return NORMALIZER.Normalise<TDim>(in value);
+        }
+        internal override Double Scale(SiMeasure measure, in Double other)
+        {
+            return measure.Anchor.Scale<TDim>(NORMALIZER, in other);
+        }
     }
 }
